7b051a05bb61c7eef4d4267d22b1a4a055ae49e3
RenanGroh <renangroh@gmail.com>
2025-11-14 12:44:17 -0300

    feat: add student array and basic operations

--- student.h
+++ student.h
@@ -18,5 +18,16 @@ typedef struct {
     size_t capacity;
 } StudentArray;
 
+/* Inicializa/limpa o array */
+void init_student_array(StudentArray *arr);
+void free_student_array(StudentArray *arr);
+
+/* Operações básicas */
+int add_student(StudentArray *arr, const Student *s); // return 0 OK
+void list_students(const StudentArray *arr);
+int find_student_index_by_id(const StudentArray *arr, int id); // -1 se não
+int remove_student_by_id(StudentArray *arr, int id); // 1 se removido
+void print_student(const Student *s);
+void prompt_student(Student *s);
 
 #endif /* STUDENT_H */
